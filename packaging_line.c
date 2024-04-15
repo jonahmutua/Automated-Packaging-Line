110db3d2886f40631bc2c30aacd6480b74832834
jonahmutua <82975985+jonahmutua@users.noreply.github.com>
2024-04-15 23:38:00 +0300

    Update packaging_line.c

--- packaging_line.c
+++ packaging_line.c
@@ -1,3 +1,25 @@
 
 #include "packaging_line.h"
 
+ /*this function block is used to detect rising/falling edge of input signal
+  *@param edgeDetectorDeviceptr - pointer to edge detector device structure
+  *@detectFallingEdge - BOOL  - variable that tells the function wether to
+  *detect risingedge / fallingEdge
+  *OUTPUT: bOut(BOOL);
+  */
+ void FB_edgeDetector(edgeDetectorDevice *edgeDetectorDeviceptr, BOOL detectFallingEdge) {
+    edgeDetectorDeviceptr->Q=FALSE;// reset egdedetctor device output
+	 
+	 if(edgeDetectorDeviceptr->CLK != edgeDetectorDeviceptr->previousIN){ /*current state of input singal differ from previous state?*/
+		 if(detectFallingEdge){/*detect falling edge*/
+			 if(edgeDetectorDeviceptr->CLK==FALSE)     edgeDetectorDeviceptr->Q=TRUE;
+			
+			 }else {/*detect rising edge*/
+		       
+			  if(edgeDetectorDeviceptr->CLK==TRUE) edgeDetectorDeviceptr->Q=TRUE;
+				   
+			      }
+		  
+	 }
+       edgeDetectorDeviceptr->previousIN=edgeDetectorDeviceptr->CLK; //update previousState of signal to current state
+ }
